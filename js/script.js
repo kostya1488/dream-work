e9ae91cb5e054fe7bc9f2f0e77b9d3a5c8f327bd
kostya <matvienkoigor956@gmail.com>
2020-07-07 16:40:18 +0300

    edited script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-$('a[href*="#"]').on('click', function(e) {
-    e.preventDefault();
+$(function() {
 
-    $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-    }, 400, 'linear');
+    $('.some_link').on('click', function(e) {
+        $('html,body').stop().animate({ scrollTop: $($(this).attr('href')).offset().top }, 1000);
+        e.preventDefault();
+    });
 });
-// **********************************************************************
+// **************************************************
 function ajaxFormRequest(form_id, form_area_id) {
     var forma = $("#" + form_id);
     var form_area = $("#" + form_area_id);
